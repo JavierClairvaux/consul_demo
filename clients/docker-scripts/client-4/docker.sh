e84db7001efe015b39cc5ac0725ed1a5dba97bf5
JavierClairvaux <javcabr@gmail.com>
2019-11-07 15:23:50 -0800

    changed for influxdb config

--- clients/docker-scripts/client-4/docker.sh
+++ clients/docker-scripts/client-4/docker.sh
@@ -1,6 +1,10 @@
-#client3 and client4
-sudo docker run --rm -d -p 8006:8443  \
-    -v $(pwd):/opt/imposter/config \
-    outofcoffee/imposter-openapi:0.7.0
-
-sudo docker run -d --network host --name payment-proxy javier1/consul-envoy -sidecar-for payment
+#Install influxdb
+sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+source /etc/lsb-release
+echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+sudo apt-get update
+sudo apt-get install influxdb -y
+sudo systemctl start influxd
+sudo apt install influxdb-client
+influx -execute "create database telegraf
+create user telegraf with password 'pass'"
