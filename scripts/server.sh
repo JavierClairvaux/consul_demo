2dafcda00bfeef25e658366e4d1126d44d760e7d
JavierClairvaux <javcabr@gmail.com>
2019-12-18 09:52:06 -0800

    deleted comments

--- scripts/server.sh
+++ scripts/server.sh
@@ -86,18 +86,6 @@ sudo add-apt-repository \
    stable"
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-
-# #Install influxdb
-# sudo curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-# source /etc/lsb-release
-# echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-# sudo apt-get update
-# sudo apt-get install influxdb -y
-# sudo systemctl start influxd
-# sudo apt install influxdb-client
-# influx -execute "create database telegraf
-# create user telegraf with password 'pass'"
-
 #Install telegraf
 cat <<EOF | sudo tee /etc/apt/sources.list.d/influxdata.list
 deb https://repos.influxdata.com/ubuntu bionic stable
